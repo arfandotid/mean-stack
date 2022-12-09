99eaa4cd7004d8ea367889a5ac8b4a817421232f
arfandotid <arfandotid@gmail.com>
2022-12-09 13:50:34 +0700

    add pagination

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,3 +1,4 @@
+import { PageEvent } from '@angular/material/paginator';
 import { PostsService } from './../posts.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Post } from '../post.model';
@@ -11,23 +12,36 @@ import { Subscription } from 'rxjs';
 export class PostListComponent implements OnInit, OnDestroy{
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1,2,5,10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.postsSub = this.postsService.getPostUpdateListener()
-      .subscribe((posts: Post[]) => {
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
         this.isLoading = false;
-        this.posts = posts;
+        this.totalPosts = postData.postCount;
+        this.posts = postData.posts;
       });
   }
 
+  onChangedPage(pageData: PageEvent) {
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
+  }
+
   onDelete(postId: string) {
-    console.log(postId);
-    this.postsService.deletePost(postId);
+    this.isLoading = true;
+    this.postsService.deletePost(postId).subscribe(() => {
+      this.postsService.getPosts(this.postsPerPage, this.currentPage);
+    });
   }
 
   ngOnDestroy(): void {
